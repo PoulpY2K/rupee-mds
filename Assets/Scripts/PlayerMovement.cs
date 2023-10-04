6ece9be41570b770bb9017992d2f0be8be648c16
Jérémy Laurent <poulpy2k@gmail.com>
2023-10-04 11:57:50 +0200

    feat(player): add movement with input

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector2 _movement;
+    private Rigidbody2D _rigidbody2D;
+
+    public float speed = 10f;
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        Debug.Log("start");
+    }
+
+    private void Update()
+    {
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        var vertical = Input.GetAxisRaw("Vertical");
+        _movement = new Vector2(horizontal, vertical).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody2D.velocity = _movement * speed;
+    }
+}
