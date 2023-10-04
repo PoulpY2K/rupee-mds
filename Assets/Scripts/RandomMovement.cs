095caf615fb248a019ed926d2f0aecf355bf8eb1
Jérémy Laurent <poulpy2k@gmail.com>
2023-10-04 15:32:15 +0200

    feat(rupee): add random rupee

---
+++ Assets/Scripts/RandomMovement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class RandomMovement : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;
+
+    public float speed = 9f;
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        var x = Random.Range(-1f, 1f);
+        var y = Random.Range(-1f, 1f);
+        var movement = new Vector2(x, y).normalized;
+        _rigidbody2D.velocity = movement * speed;
+    }
+}
