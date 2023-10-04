007550b207e7fabda8abadf03c60c644334fecf5
Jérémy Laurent <poulpy2k@gmail.com>
2023-10-04 14:11:09 +0200

    feat(animation): walking animation working

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,13 +7,18 @@ public class PlayerMovement : MonoBehaviour
 {
     private Vector2 _movement;
     private Rigidbody2D _rigidbody2D;
+    private Animator _animator;
+    
+    private static readonly int Horizontal = Animator.StringToHash("Horizontal");
+    private static readonly int Vertical = Animator.StringToHash("Vertical");
+    private static readonly int Speed = Animator.StringToHash("Speed");
 
-    [Range(5f, 30f)]   
-    public float speed = 10f;
-
+    [Range(5f, 30f)] public float speed = 10f;
+    
     private void Awake()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     private void Start()
@@ -25,7 +30,11 @@ public class PlayerMovement : MonoBehaviour
     {
         var horizontal = Input.GetAxisRaw("Horizontal");
         var vertical = Input.GetAxisRaw("Vertical");
+
         _movement = new Vector2(horizontal, vertical).normalized;
+        _animator.SetFloat(Horizontal, horizontal);
+        _animator.SetFloat(Vertical, vertical);
+        _animator.SetFloat(Speed, _movement.sqrMagnitude);
     }
 
     private void FixedUpdate()
